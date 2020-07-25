f625a4dab905b7e448a5c9d9fb6be082435c3cc6
Yash Datta <ydatta@asu.edu>
2020-07-25 19:48:16 +0800

    Add d3-graph-react dependency

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Graph } from "react-d3-graph";
+
+const welcome = { greeting: 'Hey', title: 'React',
+};
+
+// graph payload (with minimalist structure)
+const data = {
+  nodes: [{ id: "Harry" }, { id: "Sally" }, { id: "Alice" }],
+  links: [
+      { source: "Harry", target: "Sally" },
+      { source: "Harry", target: "Alice" },
+  ],
+};
+
+// the graph configuration, you only need to pass down properties
+// that you want to override, otherwise default ones will be used
+const myConfig = {
+  nodeHighlightBehavior: true,
+  node: {
+      color: "lightgreen",
+      size: 120,
+      highlightStrokeColor: "blue",
+  },
+  link: {
+      highlightColor: "lightblue",
+  },
+};
+
+function getTitle(title) {
+  return title;
+}
+
+const list = [
+  {
+title: 'React',
+url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+points: 4,
+objectID: 0,
+}, {
+title: 'Redux',
+url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+points: 5,
+objectID: 1,
+}, ];
+
+function List() {
+  return list.map(function(item) {
+  return (
+  <div key={item.objectID}>
+  <span>
+  <a href={item.url}>{item.title}</a>
+  </span> <span>{item.author}</span> <span>{item.num_comments}</span> <span>{item.points}</span>
+  </div> );
+  });
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <List />
+      <Graph
+    id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+    data={data}
+    config={myConfig}
+    />;
     </div>
   );
 }
