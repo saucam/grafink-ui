6770cc63f9ba1bc89b0b5063f42f284a49d0ff3c
Yash Datta <ydatta@asu.edu>
2020-07-29 10:10:12 +0800

    Fix rest call

--- src/Rest.js
+++ src/Rest.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 export function send_to_server(gremlin_query, address, callback) { 
-    const server_url = `http://${address.host}:${address.port}`;
-    const bodyString = "{ \"gremlin\": \"" + gremlin_query + "\" }";
-    console.log(bodyString)
-    fetch(server_url, {
-        method: 'post',
-        mode: 'no-cors',
-        body: bodyString
-    }).then(function(response) {
-        console.log(response)
-    }); 
+    const server_url = `http://${address.host}:${address.port}?ts=TestJanusGraph.traversal()`;
+    (async () => {
+        let response = await fetch(server_url, {
+            method: 'post',
+            accept: "application/json",
+            body: JSON.stringify({"gremlin" : gremlin_query})
+        });
+        let result = await response.json();
+        console.log(result);
+    })();
+    // fetch(server_url, {
+    //     method: 'post',
+    //     mode: 'no-cors',
+    //     body: bodyString
+    // }).then(function(response) {
+    //     response.json()
+    // }).then(function(data) {
+    //     console.log(data)
+    // });
 }
